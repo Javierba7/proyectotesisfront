56743863aaf33c51f9a5ec3fa8abc61ce52e8901
JavierBa <78235573+Javierba7@users.noreply.github.com>
2021-10-13 01:11:41 -0600

    creating navbar

---
+++ src/components/Dashboard/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../../css/dashboard.css';
+
+import Navbar from '../Navbar';
+
+const Dashboard = () => {
+    return (
+        <div>
+            <Navbar />
+            test
+        </div>
+    )
+}
+
+export default Dashboard;
---
+++ src/components/Navbar/index.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min';
+
+import '../../css/navbar.css';
+
+const NavbarFunction = () => {
+
+    const cleanLocal = () => {
+        localStorage.clear();
+    }
+
+    return (
+        <div>
+        <nav className="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
+                <Link className="navbar-brand" to="/">Miscelaneos Sandra</Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav tml-auto">
+                    <li className="nav-item">
+                        <input type="text" name="searchBtn" id="searchInput"/>
+                        <label id="searchBtn">Buscar</label>
+                    </li>
+                    <li className="nav-item">
+                       <Link className="nav-link" to="/">Perfil</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link  className="nav-link" to="/">Carrito</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link" onClick={cleanLocal} to="/">Cerrar Sesión</Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+    )
+}
+
+export default NavbarFunction
